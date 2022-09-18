ce94d5dbd2703526184f552fca1e149762564f5d
Gaspard <gaspard.dama@gmail.com>
2022-09-18 20:18:26 +0200

    Added a working (more or less) authorization system

--- website/views.py
+++ website/views.py
@@ -4,6 +4,7 @@ import random
 import string
 
 from flask import Flask, render_template, redirect, request, send_file, session, url_for
+import dotenv
 
 from website import utils
 
@@ -12,6 +13,8 @@ sessions = []
 
 app = Flask(__name__)
 app.config.from_object("config")
+dotenv.load_dotenv()
+
 utils.autodelete_sessions(sessions)
 
 print("================== pass", os.environ.get("password"))
@@ -21,32 +24,34 @@ print("================== pass", os.environ.get("password"))
 def root():
     return redirect(url_for("login"))
 
-@app.route("/unauthorized/  ")
+@app.route("/unauthorized/")
 def unauthorized():
     return render_template("unauthorized.html", message="Sorry, an error occured.")
 
 
 @app.route("/login/", methods=["GET", "POST"])
 def login():
-    # if utils.session_is_valid(request.cookies.get("session-token"), sessions):
-    #     return redirect(url_for("files", directory="Desktop"))
+    if utils.session_is_valid(session.get("session-token"), sessions):
+        return redirect(url_for("files", path="Desktop"))
 
     if request.method == "POST":
         password = request.form.get("password")
 
-        if password != '1234':
+        if password != os.environ.get("PASSWORD"):
             return render_template("unauthorized.html", message="Sorry, but the password is incorrect.")
 
         else:
             session_token = ''.join([random.choice(string.printable) for _ in range(32)])
 
             # 1h session duration
-            session_expire = datetime.datetime.now() + datetime.timedelta(hours=1)
+            session_expire = datetime.datetime.now() + datetime.timedelta(hours=4)
 
             sessions.append(utils.Session(session_expire, session_token))
 
+            # Writing the session token to the user's session storage
+            session["session-token"] = session_token
+
             response = redirect(url_for("files", path="Desktop"))
-            response.set_cookie("session-token", session_token)
 
             return response
 
@@ -60,6 +65,9 @@ def files_root():
 
 @app.route("/files/<path:path>")
 def files(path):
+    if not utils.session_is_valid(session.get("session-token"), sessions):
+        return render_template("unauthorized.html", message="Sorry but your session is invalid.")
+
     path = path.replace("..", ".").replace("~", ".")  # Securing the path
 
     if not path.startswith("Desktop"):
@@ -70,13 +78,9 @@ def files(path):
     if os.path.isfile(utils.BASE_DIR + path):
         return send_file(utils.BASE_DIR + path, as_attachment=True)
 
-
     elif not os.path.exists(utils.BASE_DIR + path):
         return "doesn't exist"
 
-    # token = session.get("session-token")
-    # if not utils.session_is_valid(token, sessions):
-    #     return render_template("unauthorized.html", message="Sorry but your session is invalid.")
 
     files, folders = utils.get_dir_content(utils.BASE_DIR + path)
 
